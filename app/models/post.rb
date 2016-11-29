00cfd342b8f7425dea8316f837d8a8440771fb59
Anthony Surganov <anthony.surganov@thirdwavellc.com>
2016-11-29 15:15:59 -0600

    added pg_search functionality to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :custom_search,
+    :against => {
+      :title => 'A',
+      :body => 'B'
+    },
+    :using => {
+      :tsearch => {:prefix => true}
+    }
 end
+
+
+# To use search, simply type results = Post.custom_search("foo") and
+# it will populate and array of objects that match your search. 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712130514) do
+ActiveRecord::Schema.define(version: 20161129210039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20160712130514) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
